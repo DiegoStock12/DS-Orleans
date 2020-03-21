5d16db67fc509f8baf8fd22e40316f1eacef9c6c
Jorisquist <Jorisquist@gmail.com>
2020-03-21 15:06:41 +0100

    Gave grains an id

--- src/main/scala/org/orleans/developer/AccountGrain.scala
+++ src/main/scala/org/orleans/developer/AccountGrain.scala
@@ -3,7 +3,7 @@ package org.orleans.developer
 import org.orleans.silo.Services.Grain.Grain
 
 
-class AccountGrain extends Grain{
+class AccountGrain(_id: String) extends Grain(_id){
     def store(): Unit = {
       println("Executing store method for account grain")
     }
