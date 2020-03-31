3bb856b326f219c536212be3ef5e0177a9c592a8
Wouter Zorgdrager <zorgdragerw@gmail.com>
2020-03-31 15:47:46 +0200

    Fix for concurrent problem.

--- src/main/scala/org/orleans/silo/dispatcher/ClientReceiver.scala
+++ src/main/scala/org/orleans/silo/dispatcher/ClientReceiver.scala
@@ -22,7 +22,9 @@ class ClientCleanup(clientSockets: util.List[MessageReceiver])
     val toRemove: util.List[MessageReceiver] =
       new util.ArrayList[MessageReceiver]()
 
-    val iterator: util.Iterator[MessageReceiver] = clientSockets.iterator()
+    val iterator: util.Iterator[MessageReceiver] =
+      new util.ArrayList(clientSockets).iterator()
+
     while (iterator.hasNext) {
       val client = iterator.next()
       if (!client.isRunning()) {
