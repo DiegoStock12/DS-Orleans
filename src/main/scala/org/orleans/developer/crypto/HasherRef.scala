27655f92b7f71ea51102c55fadebd2cec52f2234
Jorisquist <Jorisquist@gmail.com>
2020-04-03 12:52:59 +0200

    Small example with multiple grains trying to find hash message

---
+++ src/main/scala/org/orleans/developer/crypto/HasherRef.scala
@@ -0,0 +1,20 @@
+package org.orleans.developer.crypto
+
+import com.sun.net.httpserver.Authenticator.Success
+import org.orleans.developer.crypto.CryptoMessages.{FindMessage, MessageFound, MessageNotFound}
+import org.orleans.silo.Services.Grain.GrainReference
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+import scala.util.Success
+
+class HasherRef extends GrainReference {
+
+  def findHash(startValue: Int, endValue: Int, hash: String): Future[Option[Int]] = {
+    (this.grainRef ? FindMessage(startValue, endValue, hash)).map {
+      case MessageFound(value, hash) => Some(value)
+      case MessageNotFound(hash) => None
+    }
+  }
+
+}
